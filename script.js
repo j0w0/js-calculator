8c214cff72904122993b2940067d62919f1d8060
Josh Woodcock <josh@j0w0.com>
2020-11-26 14:33:34 -0600

    Initial commit - HTML and set up Calculator class

---
+++ script.js
@@ -0,0 +1,50 @@
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+    this.clear();
+  }
+
+  clear() {
+    this.currentOperand = '';
+    this.previousOperand = '';
+    this.operation = undefined;
+  }
+
+  delete() {
+
+  }
+
+  appendNumber(number) {
+
+  }
+
+  chooseOperation(operation) {
+
+  }
+
+  compute() {
+
+  }
+
+  updateDisplay() {
+    
+  }
+}
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+const calc = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calc.appendNumber(button.innerText);
+    calc.updateDisplay();
+  });
+});
